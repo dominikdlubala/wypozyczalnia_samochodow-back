ecd84d55973925ac761064d7741dda54ff96267d
Dominik Dłubała <ddlubala15@gmail.com>
2024-10-09 19:36:05 +0200

    first commit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3211ce69d92c58fe70b116ba97ca83468afcf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c10124a44c8793618ea8aab6a6005af4ec93f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
