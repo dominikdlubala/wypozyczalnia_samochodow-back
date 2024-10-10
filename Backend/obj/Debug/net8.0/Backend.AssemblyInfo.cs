b2d0b4f02c133a45eca6595ea4ec6722c392b401
Dominik Dłubała <ddlubala15@gmail.com>
2024-10-10 21:52:42 +0200

    commit fix, bin obj removed

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd84d55973925ac761064d7741dda54ff96267d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee1c39702e9e47ef70d8d31bfcbd2618b21a72b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
