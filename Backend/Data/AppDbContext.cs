efba4c18da0a38cebfeafebfb5c40501efdbed60
Kruzik02 <152690148+Kruzik02@users.noreply.github.com>
2024-10-15 17:49:10 +0200

    Dodanie Modeli

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -7,6 +7,10 @@ public class AppDbContext: DbContext {
     public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
 
     public DbSet<User> Users {get; set;}
+    public DbSet<Car> Cars {get; set;}
+    public DbSet<Fault> Faults {get; set;}
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Review> Reviews {get; set;}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
---
+++ Backend/Models/Fault.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Models
+{
+    public class Fault
+    {
+        public int Id { get; set; }
+        public int ReportedUserId { get; set; }
+        public int CardId { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfIssue { get; set; }
+
+        public virtual Car Car { get; set; }
+        public virtual User ReportedUser { get; set; }
+    }
+}
