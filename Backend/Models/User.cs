7252c4efb2c50033ff0bf16fade760ee1e8fb64c
Krystian Ochmański <krysochmanski1337@gmail.com>
2024-10-16 23:34:58 +0200

    updated User class

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -7,13 +7,17 @@ public class User {
     public int Id { get; set;}
     public string? FirstName { get; set;}    
     public string? LastName { get; set;}
-    public DateTime RegistrationDate { get; set;}
+    public DateTime RegistrationDate { get; set;} = DateTime.Now;
+
+    [Required(ErrorMessage = "Email is required")]
+    public string Email { get; set; } = null!;
     
     [Required(ErrorMessage = "Username is required")]
     public string Username { get; set; } = null!;
     
     [Required(ErrorMessage = "Password is required")]
     public string Password {get; set; } = null!;
+    public bool IsAdmin { get; set; } = false;
 
     public virtual List<Car>? FavouriteCars { get; set;}
     public virtual List<Reservation>? Reservations { get; set;}
