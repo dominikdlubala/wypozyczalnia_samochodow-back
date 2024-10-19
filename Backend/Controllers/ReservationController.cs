b03587e3ac3a188af1a26a6c2875844260026916
dominikdlubala <ddlubala15@gmail.com>
2024-10-19 15:44:56 +0200

    ReservationController added, integrated some of the actions with frontend

---
+++ Backend/Controllers/ReservationController.cs
@@ -0,0 +1,110 @@
+using System.Net;
+using Backend.Data;
+using Backend.Models;
+using Backend.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReservationController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ReservationController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllReservations()
+        {
+            try {
+                var reservations =  await _context.Reservations.ToListAsync();
+                return Ok(reservations);
+            } catch (Exception e) {
+                return BadRequest(e); 
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReservationById(int id)
+        {
+            try {
+                var reservation = await _context.Reservations.FindAsync(id);
+
+                if (reservation == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(reservation);
+            } catch (Exception e){
+                return BadRequest(e); 
+            }
+        }
+
+        [HttpGet("user{userId}")]
+        public async Task<IActionResult> GetUserReservationsById(int userId)
+        {
+            try {
+                var reservations = await _context.Reservations.AsQueryable().Where(r => r.UserId == userId).ToListAsync();
+
+                if (reservations == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(reservations);
+            } catch (Exception e){
+                return BadRequest(e); 
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddReservation([FromBody] Reservation newReservation)
+        {
+            try {
+                if (newReservation == null)
+                {
+                    return BadRequest("Car object is null");
+                }
+                var car = await _context.Cars.FindAsync(newReservation.CarId); 
+
+                Console.WriteLine($"Received reservation: CarId={newReservation.CarId}, StartDate={newReservation.StartDate}, EndDate={newReservation.EndDate}");
+                if(car == null) return BadRequest("Car not found"); 
+                newReservation.Car = car; 
+                await _context.Reservations.AddAsync(newReservation);
+                await _context.SaveChangesAsync();
+
+                return Ok(newReservation);
+            } catch (Exception e){
+                return BadRequest(e); 
+            }
+        }
+
+
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteCar(int id)
+        // {
+        //     try {
+        //         var car = _context.Cars.Find(id);
+
+        //         if (car == null)
+        //         {
+        //             return NotFound();
+        //         }
+
+        //         _context.Cars.Remove(car);
+        //         await _context.SaveChangesAsync();
+
+        //         return NoContent();
+        //     } catch (Exception e){
+        //         return BadRequest(e); 
+        //     }
+        // }
+
+    }
+}
