3aecbc73bd7af0c5b8f5236086518430326365ce
Krystian Ochmański <krysochmanski1337@gmail.com>
2024-11-26 15:35:58 +0100

    added update user and change password

---
+++ Backend/Models/DTOs/ChangePasswordDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.DTOs
+{
+    public class ChangePasswordDTO
+    {
+        // Jeśli UserId jest niepotrzebny (można pobrać z kontekstu), usuń go
+        // public int UserId { get; set; }
+
+        [Required]
+        public string CurrentPassword { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Nowe hasło musi mieć co najmniej 8 znaków.")]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/Models/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.DTOs
+{
+    public class UpdateUserDTO
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+        [EmailAddress]
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+    }
+}
