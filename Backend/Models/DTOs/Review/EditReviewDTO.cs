b48591107773fe824573f17210c0d0029033989c
Krystian Ochmański <krysochmanski1337@gmail.com>
2025-01-08 16:38:27 +0100

    One review per user done

---
+++ Backend/Models/DTOs/Review/EditReviewDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Models.DTOs.Review
+{
+    public class EditReviewDTO
+    {
+        public float StarsOutOfFive { get; set; }
+        public string ReviewContent { get; set; } = null!;
+    }
+}
